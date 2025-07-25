eeebca748058cbdd0acd8a0fee397421b75b887e
German Avakjan <german.avakjan@inbank.ee>
2025-07-25 15:24:30 +0300

    Implemented task list functionality

--- src/main/java/com/internshipGoals/tasksApp/controllers/TaskListController.java
+++ src/main/java/com/internshipGoals/tasksApp/controllers/TaskListController.java
@@ -1,10 +1,13 @@
 package com.internshipGoals.tasksApp.controllers;
 
 import com.internshipGoals.tasksApp.domain.dto.TaskListDto;
+import com.internshipGoals.tasksApp.domain.entities.TaskList;
 import com.internshipGoals.tasksApp.mappers.TaskListMapper;
 import com.internshipGoals.tasksApp.services.TaskListService;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +30,13 @@ public class TaskListController {
         .map(taskListMapper::toDto)
         .toList();
   }
+
+  @PostMapping
+  public TaskListDto createTaskList(@RequestBody TaskListDto taskListDto) {
+    TaskList createdTaskList = taskListService.createTaskList(
+        taskListMapper.fromDto(taskListDto)
+    );
+    return taskListMapper.toDto(createdTaskList);
+  }
+
 }
--- src/main/java/com/internshipGoals/tasksApp/mappers/impl/TaskListServiceImpl.java
+++ src/main/java/com/internshipGoals/tasksApp/mappers/impl/TaskListServiceImpl.java
@@ -3,6 +3,7 @@ package com.internshipGoals.tasksApp.mappers.impl;
 import com.internshipGoals.tasksApp.domain.entities.TaskList;
 import com.internshipGoals.tasksApp.repositories.TaskListRepository;
 import com.internshipGoals.tasksApp.services.TaskListService;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +19,25 @@ public class TaskListServiceImpl implements TaskListService {
   @Override
   public List<TaskList> listTaskLists() {
     return taskListRepository.findAll();
+  }
+
+  @Override
+  public TaskList createTaskList(TaskList taskList) {
+    if (null != taskList.getId()) {
+      throw new IllegalArgumentException("Task list already exists");
+    }
+    if (null == taskList.getTitle() || taskList.getTitle().isBlank()) {
+      throw new IllegalArgumentException("Task list title cannot be empty");
+    }
+
+    LocalDateTime now = LocalDateTime.now();
+    return taskListRepository.save(new TaskList(
+        null,
+        taskList.getTitle(),
+        taskList.getDescription(),
+        null,
+        now,
+        now
+    ));
   }
 }
